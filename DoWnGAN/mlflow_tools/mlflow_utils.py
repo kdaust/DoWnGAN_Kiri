68ad2a68c6b4c593f49531fb0c4c6608317f5dfc
Kiri Daust <34073038+kdaust@users.noreply.github.com>
2022-10-31 22:05:36 -0700

    fixed i hope

--- DoWnGAN/mlflow_tools/mlflow_utils.py
+++ DoWnGAN/mlflow_tools/mlflow_utils.py
@@ -14,40 +14,10 @@ def define_experiment(mlclient):
     print("Enter the experiment name you wish to add the preceding training run to.")
     print("Select number from list or press n for new experiment: ")
     [print(exp.experiment_id,":", exp.name) for i, exp in enumerate(mlclient.list_experiments())]
-    again = False
-    while not again:
-       # choice = input("Input number here: ")
-	choice = "n"
-        if choice == "n":
-            g = True
-            while g:
-                set_exp = "Test_experiment"
-		confirm = input(f"You entered {set_exp}. Happy? (Y/n) ")
-                g = False
-		if confirm in ["Y", '']:
-                    mlclient.create_experiment(set_exp)
-                    g = False
-            again = True
-
-        elif choice.isnumeric():
-            g = True
-            set_exp = mlclient.get_experiment(choice).name
-            while g:
-                confirm = input(f"You have selected {set_exp}. Happy? (Y/n) ")
-                if confirm in ["Y", '']:
-                    g = False
-            again = True
-
-        else:
-            print("Please select a valid input")
-
+    set_exp = "Test_Experiment"
+    mlclient.create_experiment(set_exp)
     return mlclient.get_experiment_by_name(set_exp).experiment_id
 
 def write_tags():
-    g = True
-    while g:
-        choice = "Let's see  if this works"
-        confirm = input(f"You entered {choice}. Happy? (Y/n) ")
-        if confirm in ["Y", '']:
-            g = False
+    choice = "This is testing..."
     return choice
