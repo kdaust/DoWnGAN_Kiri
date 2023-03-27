a70db085942a409241c094f503b4d319e9e942f4
Kiri Daust <34073038+kdaust@users.noreply.github.com>
2023-03-26 21:38:56 -0700

    marginal

--- DoWnGAN/Compare_Marginal.py
+++ DoWnGAN/Compare_Marginal.py
@@ -14,29 +14,35 @@ device = torch.device("cuda:0")
 #mod_noise = "/media/data/mlflow_exp/4/9625d9c4e7584218827d4ec1740eb7f0/artifacts/Generator/Generator_500"
 #G = mlflow.pytorch.load_model(mod_noise)
 
-data_folder = "/home/kiridaust/Masters/Data/processed_data/ds_wind/"
+data_folder = "/home/kiridaust/Masters/Data/ToyDataSet/"
 
-cond_fields = xr.open_dataset(data_folder + "coarse_validation.nc", engine="netcdf4")
-coarse = torch.from_numpy(cond_fields.to_array().to_numpy()).transpose(0, 1).to(device).float()
-invariant = xr.open_dataset(data_folder + "DEM_Crop.nc", engine = "netcdf4")
-invariant = torch.from_numpy(invariant.to_array().to_numpy()).to(device).float()
+# cond_fields = xr.open_dataset(data_folder + "coarse_validation.nc", engine="netcdf4")
+# coarse = torch.from_numpy(cond_fields.to_array().to_numpy()).transpose(0, 1).to(device).float()
+# invariant = xr.open_dataset(data_folder + "DEM_Crop.nc", engine = "netcdf4")
+# invariant = torch.from_numpy(invariant.to_array().to_numpy()).to(device).float()
+coarse = np.load(data_folder+"coarse_val.npy")
+coarse = np.swapaxes(coarse, 0, 2)
+coarse = torch.from_numpy(coarse)[:,None,...].to(device).float()
+fine = np.load(data_folder+"fine_val.npy")
+fine = np.swapaxes(fine, 0, 2)
+fine = torch.from_numpy(fine)[:,None,...]
 
 batchsize = 50
-invariant = invariant.repeat(batchsize,1,1,1)
+#invariant = invariant.repeat(batchsize,1,1,1)
 # noise_f = torch.normal(0,1,size = [batchsize,1,128,128], device=device)
 # invariant = torch.cat([invariant, noise_f], 1)
 # print(invariant.size())
 
-sample = 42
+sample = 0
 coarse_in = coarse[sample,...]
 print(coarse_in.size())
 coarse_in = coarse_in.unsqueeze(0).repeat(batchsize,1,1,1)
 
-models = ['28374f6f7190495cbf92d698584a2da2','65e9cd4ba68045bdb79526d0196b654e']
+models = ['a04652b7943a492d8cb7f6cacefb9f11','ccfb1a914fcd43c58aae2ef2c27a54ef']
 modNm = ['CovLR_PFS','Inject_PFS']
 #modDat = [coarse_noise, coarse_in]
-xp = 64
-yp = 20
+xp = 120
+yp = 120
 
 res = dict()
 for i in range(len(modNm)):
@@ -48,9 +54,9 @@ for i in range(len(modNm)):
         curr_coarse = torch.cat([coarse_in, noise_c], 1)
     else:
         curr_coarse = coarse_in
-    gen_out = G(curr_coarse,invariant).cpu().detach()
+    gen_out = G(curr_coarse).cpu().detach()
     for j in range(20):
-        fine_gen = G(curr_coarse,invariant)
+        fine_gen = G(curr_coarse)
         gen_out = torch.cat([gen_out,fine_gen.cpu().detach()],0)
         del fine_gen
     
@@ -60,6 +66,6 @@ for i in range(len(modNm)):
     
 for nm in modNm:
     sns.kdeplot(res[nm], label = nm)
-
+sns.kdeplot(fine[:,0,xp,yp].flatten(),label = "real")
 plt.legend()
-plt.savefig("Marginal_Compare2.png", dpi = 600)
+plt.savefig("Marginal_Compare_Toydat.png", dpi = 600)
