2beabcf2cfd2241d26db7faadef12915590b2e97
Kiri Daust <kiridaust@gmail.com>
2023-04-06 22:37:09 -0700

    working on mean stoch

--- DoWnGAN/GAN/stage.py
+++ DoWnGAN/GAN/stage.py
@@ -22,8 +22,8 @@ data_folder = "/home/kiridaust/Masters/Data/processed_data/ds_wind/"
 
 def load_preprocessed():
     if(highres_in):
-        coarse_train = xr.open_dataset(data_folder + "coarse_train.nc", engine="netcdf4")
-        fine_train = xr.open_dataset(data_folder + "fine_train.nc", engine="netcdf4")
+        coarse_train = xr.open_dataset(data_folder + "coarse_train_sht.nc", engine="netcdf4")
+        fine_train = xr.open_dataset(data_folder + "fine_train_sht.nc", engine="netcdf4")
         coarse_test = xr.open_dataset(data_folder + "coarse_test.nc", engine="netcdf4")
         fine_test = xr.open_dataset(data_folder + "fine_test.nc", engine="netcdf4")
         invarient = xr.open_dataset(data_folder + "DEM_Crop.nc", engine="netcdf4")
