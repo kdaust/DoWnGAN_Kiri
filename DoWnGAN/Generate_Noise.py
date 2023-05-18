3689a04677f942acbd442fac671e8a6959a05772
Kiri Daust <kiridaust@gmail.com>
2023-05-18 11:45:11 -0700

    changes for dell

--- DoWnGAN/Generate_Noise.py
+++ DoWnGAN/Generate_Noise.py
@@ -55,7 +55,7 @@ crps.CRPS(GS[:,11,110].flatten(), RS[11,110].cpu()).compute()
 crps.CRPS(GR[:,11,110].flatten(), RR[11,110].cpu()).compute()
 #################################3
 ##generate temperature fields
-G = mlflow.pytorch.load_model("/media/data/mlflow_exp/4/971937cd44424b438c113ead26c4384d/artifacts/Generator/Generator_500")
+G = mlflow.pytorch.load_model("Generators/dbba1469156c44ae8ebeaa4a239ecef9/artifacts/Generator/Generator_500")
 data_folder = "/home/kiridaust/Masters/Data/processed_data/ds_temp/"
 
 cond_fields = xr.open_dataset(data_folder + "coarse_validation.nc", engine="netcdf4")
@@ -131,29 +131,46 @@ np.save("CRPS_Comp.npy",dat)
 #mod_noise = "/media/data/mlflow_exp/4/94c6d5ecb2d84eb085d424cf0c7248e3/artifacts/Generator/Generator_500"
 mod_noise = "/media/data/mlflow_exp/4/6f3f3b042510493cb385939d57864de3/artifacts/Generator/Generator_480" ##dem+10
 mod_noise = "/media/data/mlflow_exp/4/9b1ddd858c8c49b285b1cb3dd1d01172/artifacts/Generator/Generator_80"
-G = mlflow.pytorch.load_model(mod_noise)
-data_folder = "/home/kiridaust/Masters/Data/processed_data/ds_temp/"
+G = mlflow.pytorch.load_model("Generators/dbba1469156c44ae8ebeaa4a239ecef9/artifacts/Generator/Generator_500/")
+#data_folder = "/home/kiridaust/Masters/Data/processed_data/ds_temp/"
 #data_folder = "/home/kiridaust/Masters/Data/ToyDataSet/Bimodal_Synth/"
-data_folder = "/home/kiridaust/Masters/Data/Synth_DEM/Weight10/"
+data_folder = "../Data/ds_temphumid/"
 
-coarse = np.load(data_folder+"coarse_test.npy")
+coarse = np.load(data_folder+"coarse_val.npy")
 coarse = np.swapaxes(coarse, 0, 2)
 coarse = torch.from_numpy(coarse)[:,None,...].to(device).float()
-fine = np.load(data_folder+"fine_test.npy")
+fine = np.load(data_folder+"fine_val.npy")
 fine = np.swapaxes(fine, 0, 2)
 fine = torch.from_numpy(fine)[:,None,...].to(device).float()
+
+plt.imshow(coarse[4,0,...].cpu())
+plt.show()
+
 invar = np.load(data_folder+"dem_crop.npy")
 invariant = torch.from_numpy(invar)[None,...].to(device).float()
 
 cond_fields = xr.open_dataset(data_folder + "coarse_test.nc", engine="netcdf4")
 fine_fields = xr.open_dataset(data_folder + "fine_test.nc", engine="netcdf4")
-coarse = torch.from_numpy(cond_fields.to_array().to_numpy()).transpose(0, 1).to(device).float()
-fine = torch.from_numpy(fine_fields.to_array().to_numpy()).transpose(0, 1).to(device).float()
+coarse = torch.from_numpy(cond_fields.to_array().to_numpy()).transpose(0, 1)
+fine = torch.from_numpy(fine_fields.to_array().to_numpy()).transpose(0, 1)
 invariant = xr.open_dataset(data_folder + "DEM_Crop.nc", engine = "netcdf4")
 invariant = torch.from_numpy(invariant.to_array().to_numpy()).to(device).float()
 
-batchsize = 32
+
+batchsize = 4
 invariant = invariant.repeat(batchsize,1,1,1)
+coarse_in = coarse[0:batchsize,...].to(device).float()
+gen = G(coarse_in, invariant)
+gen.shape
+gen = gen.cpu().detach()
+plt.imshow(gen[0,0,...])
+plt.show()
+plt.close()
+plt.imshow(gen[3,1,...])
+plt.show()
+plt.imshow(fine[3,1,...])
+plt.show()
+
 # noise_f = torch.normal(0,1,size = [batchsize,1,128,128], device=device)
 # invariant = torch.cat([invariant, noise_f], 1)
 # print(invariant.size())
@@ -196,23 +213,38 @@ plt.hist(l2)
 merid = l2
 
 ###plot marginal distributions
-coarse_in = coarse[42,...]
+batchsize = 16
+coarse_in = torch.mean(coarse,0)
+plt.imshow(coarse_in[0,...].cpu())
+plt.show()
+for i in range(4):
+  plt.close()
+  plt.imshow(fine[i,0,...].cpu())
+  plt.show()
+  
 coarse_in = coarse_in.unsqueeze(0).repeat(batchsize,1,1,1)
 
-gen_out = G(coarse_in, invariant).cpu().detach()
-for i in range(5):
-    fine_gen = G(coarse_in, invariant)
+gen_out = G(coarse_in).cpu().detach()
+for i in range(4):
+  plt.close()
+  plt.imshow(gen_out[i,0,...])
+  plt.show()
+  
+for i in range(10):
+    fine_gen = G(coarse_in)
     gen_out = torch.cat([gen_out,fine_gen.cpu().detach()],0)
     del fine_gen
 
-xp = 70
-yp = 60
+
+xp = 5
+yp = 5
 samp1 = gen_out[:,0,xp,yp].flatten()
 samp2 = fine[0:192,0,xp,yp].cpu().flatten()
-sns.set_style('whitegrid')
+plt.close()
 sns.kdeplot(samp1,label = "Gen")
 sns.kdeplot(samp2,label = "Real")
 plt.legend()
+plt.show()
 
 torch.quantile(fine[0:250,...].cpu(), torch.tensor([0.01,0.1,0.5,0.9,0.99]))
 
