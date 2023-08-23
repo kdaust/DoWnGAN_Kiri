bde52da226fd75ebf1f5015c7e755d65ae03ea67
Daust <Kiri.Daust@gov.bc.ca>
2023-08-23 10:40:20 -0700

    Updates: just crps

--- DoWnGAN/mlflow_tools/mlflow_utils.py
+++ DoWnGAN/mlflow_tools/mlflow_utils.py
@@ -19,5 +19,5 @@ def define_experiment(mlclient):
     return mlclient.get_experiment_by_name(set_exp).experiment_id
 
 def write_tags():
-    choice = "(16x16) Temp stochastic, increased CRPS loss"
+    choice = "(16x16) Wind stochastic, just crps and critic"
     return choice
